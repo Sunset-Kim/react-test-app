2059ca9a6c97cfa2f133e7449de67ddb880034bd
Sunset-kim <wood2002@naver.com>
2022-10-30 21:53:53 +0900

    green: when button click, change count

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
-      <div data-testid='count'>0</div>
-      <button type='button' data-testid='btn-minus'>
+      <div data-testid='count'>{count}</div>
+      <button
+        type='button'
+        data-testid='btn-minus'
+        onClick={() => decreaseCount()}
+      >
         -
       </button>
 
-      <button type='button' data-testid='btn-plus'>
+      <button
+        type='button'
+        data-testid='btn-plus'
+        onClick={() => increaseCount()}
+      >
         +
       </button>
     </div>
