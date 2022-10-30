ec9a5025981f847d80bdcd859850625cab05a441
Sunset-kim <wood2002@naver.com>
2022-10-30 21:43:47 +0900

    red: plus, minus button render test
    App Compnent render 시에 plus, minus 버튼의 각각 text content의 값을 평가한다

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,15 @@ test("App render after count 0", () => {
   const countElement = screen.getByTestId("count");
   expect(countElement).toHaveTextContent(0);
 });
+
+test("minus button의 텍스트는 '-' 입니다.", () => {
+  render(<App />);
+  const countElement = screen.getByTestId("btn-minus");
+  expect(countElement).toHaveTextContent("-");
+});
+
+test("plus button의 텍스트는 '+' 입니다.", () => {
+  render(<App />);
+  const countElement = screen.getByTestId("btn-plus");
+  expect(countElement).toHaveTextContent("+");
+});
