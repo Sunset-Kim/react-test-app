cf38662a1aa30de5dfece1eabc7fcb79d9a05667
Sunset-kim <wood2002@naver.com>
2022-10-30 22:07:38 +0900

    green: implement display button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   const increaseCount = () => {
     setCount(count + 1);
@@ -11,12 +12,16 @@ function App() {
   const decreaseCount = () => {
     setCount(count - 1);
   };
+
+  const handleToggle = () => setDisabled(!disabled);
+
   return (
     <div>
       <div data-testid='count'>{count}</div>
       <button
         type='button'
         data-testid='btn-minus'
+        disabled={disabled}
         onClick={() => decreaseCount()}
       >
         -
@@ -25,9 +30,14 @@ function App() {
       <button
         type='button'
         data-testid='btn-plus'
+        disabled={disabled}
         onClick={() => increaseCount()}
       >
         +
+      </button>
+
+      <button type='button' data-testid='btn-display' onClick={handleToggle}>
+        on/off
       </button>
     </div>
   );
