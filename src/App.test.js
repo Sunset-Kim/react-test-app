934a17ccb1306b1663ff2c7a5360ffbb6d01d4e3
Sunset-kim <wood2002@naver.com>
2022-10-30 21:49:39 +0900

    red: 각 버튼을 누를때, count가 작동합니다.
    1. plus 버튼을 누르면, count가 1로 렌더링된다
    2. minus 버튼을 누르면, count가 -1로 렌더링된다.

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("App render after count 0", () => {
@@ -17,4 +17,22 @@ test("plus button의 텍스트는 '+' 입니다.", () => {
   render(<App />);
   const countElement = screen.getByTestId("btn-plus");
   expect(countElement).toHaveTextContent("+");
+});
+
+test("minus button을 누르면 count가 -1로 변합니다", () => {
+  render(<App />);
+  const count = screen.getByTestId("count");
+  const countElement = screen.getByTestId("btn-minus");
+  fireEvent.click(countElement);
+
+  expect(count).toHaveTextContent("-1");
+});
+
+test("plus button을 누르면 count가 1로 변합니다", () => {
+  render(<App />);
+  const count = screen.getByTestId("count");
+  const countElement = screen.getByTestId("btn-plus");
+  fireEvent.click(countElement);
+
+  expect(count).toHaveTextContent("1");
 });
