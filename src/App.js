e134719d3b418fda5447e6cfbc4d58bbf4efd612
Sunset-kim <wood2002@naver.com>
2022-10-30 21:46:25 +0900

    green: button component 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div data-testid='count'>0</div>;
+  return (
+    <div>
+      <div data-testid='count'>0</div>
+      <button type='button' data-testid='btn-minus'>
+        -
+      </button>
+
+      <button type='button' data-testid='btn-plus'>
+        +
+      </button>
+    </div>
+  );
 }
 
 export default App;
